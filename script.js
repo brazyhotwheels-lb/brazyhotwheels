519cef782f3fca43ee23e8d5fe5d58c45affe59d
brazyhotwheels-lb <matchablossom66@gmail.com>
2025-08-22 20:41:51 +0300

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const whatsappNumber = "96171226007";
 const products = [
     { id: 1, name: "Tesla Model S Plaid", price: 4, sealed: true, image: "images/tesla plaid.jpg" },
     { id: 2, name: "Dodge Viper SRT10 ACR", price: 4, sealed: true, image: "images/dodge viper.png" },
-    { id: 3, name: "'98 Subaru Impreza 22B-STi Version", price: 4, sealed: true,  image: "images/subaru.png" },
+    { id: 3, name: "'98 Subaru Impreza 22B-STi Version", price: 4, sealed: true,  image: "images/subaru.jpg" },
     { id: 4, name: "Alfa Romeo GTV6 3.0", price: 4, sealed: true,  image: "images/alfa romeo.jpg" },
     { id: 5, name: "2020 Koenigsegg Jesko", price: 4, sealed: true,  image: "images/2020 jesko.jpg" },
     { id: 6, name: "Ford Mustang Dark Horse", price: 15, sealed: true,  image: "images/ford mustang.jpg" },
@@ -66,3 +66,4 @@ document.getElementById("cart-btn").addEventListener("click", () => {
 // Initial render
 renderProducts();
 updateCartCount();
+
