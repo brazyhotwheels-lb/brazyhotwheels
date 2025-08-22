41ddf445fe7d33f4d287794c5db738560a9f2523
brazyhotwheels-lb <matchablossom66@gmail.com>
2025-08-22 23:19:57 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", () => {
  const products = [
     { id: 1, name: "Tesla Model S Plaid", price: 4, sealed: true, image: "images/tesla plaid.jpg", description: "A super fast electric car with futuristic design." },
     { id: 2, name: "Dodge Viper SRT10 ACR", price: 4, sealed: true, image: "images/dodge viper.png", description: "Classic American muscle car with insane acceleration." },
-    { id: 3, name: "'98 Subaru Impreza 22B-STi Version", price: 4, sealed: true, image: "images/image.png", description: "Iconic rally car with AWD and turbocharged power." },
+    { id: 3, name: "'98 Subaru Impreza 22B-STi Version", price: 4, sealed: true, image: "images/subaru.jpg", description: "Iconic rally car with AWD and turbocharged power." },
     { id: 4, name: "Alfa Romeo GTV6 3.0", price: 4, sealed: true, image: "images/alfa romeo.jpg", description: "Italian classic sports coupe with sleek design." },
     { id: 5, name: "2020 Koenigsegg Jesko", price: 4, sealed: true, image: "images/2020 jesko.jpg", description: "Hypercar with extreme speed and performance." },
     { id: 6, name: "Ford Mustang Dark Horse", price: 15, sealed: true, image: "images/ford mustang.jpg", description: "Modern muscle car with bold styling." },
@@ -62,4 +62,5 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 
 });
+
 
